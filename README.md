780c3fecaf8240267f7df38904b407003b722d56
joe27g <20873239+joe27g@users.noreply.github.com>
2025-05-20 19:56:38 -0500

    add configs & supporting files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # eslint-config-typescript
-An ESLint config for Typescript with a handful of basic rules.
+
+An ESLint config for Typescript including Prettier and a handful of basic rules.
+
+Also includes Prettier and VS Code config files for convenience.
---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import prettier from 'eslint-config-prettier/flat';
+
+export default defineConfig([
+    js.configs.recommended,
+    ts.configs.strict,
+    prettier,
+    {
+        languageOptions: {
+            parser: ts.parser,
+            globals: globals.node,
+        },
+
+        rules: {
+            '@typescript-eslint/no-unused-vars': 'warn',
+            '@typescript-eslint/no-empty-function': 'warn',
+            'no-useless-escape': 'warn',
+            'no-empty': 'warn',
+            'no-var': 'error',
+            'prefer-const': 'warn',
+            'no-fallthrough': 'warn',
+            '@typescript-eslint/no-explicit-any': 'warn',
+            '@typescript-eslint/consistent-type-imports': 'warn',
+            'no-console': 'warn',
+        },
+    },
+    globalIgnores([
+        '**/logs',
+        '**/*.log',
+        '**/pids',
+        '**/*.pid',
+        '**/*.seed',
+        '**/node_modules',
+        '**/.DS_Store',
+        '**/.eslint.config.*',
+        '**/dist',
+    ]),
+]);
